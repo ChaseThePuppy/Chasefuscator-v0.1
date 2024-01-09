66af41a4ae7577db55125c479b14b3b8c39ac3bb
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-01-09 21:39:20 +0500

    Create main-log-printer.lua

---
+++ main-log-printer.lua
@@ -0,0 +1,8 @@
+--------------
+---Made By----
+---Chase  ----
+---The    ----
+---Puppy  ----
+--------------
+
+return(function() local Chasefuscator = setmetatable end return(function() [2de] = connect.Server{"Sa?","u","n","are","yo","watch","u","me","ing"} = connect.server.text() local a = [werr].[sds].[35], local 2de = print(text) return(Main-Log-Printer ()() end end end end end end
