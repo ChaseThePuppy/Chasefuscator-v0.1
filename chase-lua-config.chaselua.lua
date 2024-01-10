4d8cc9c98ee577fe9b8708f16eed9a9df73a86ed
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-01-10 21:35:36 +0500

    Create chase-lua-config.chaselua.lua

---
+++ chase-lua-config.chaselua.lua
@@ -0,0 +1,11 @@
+--file to config for chaselua runner
+------------------------------------
+---       ChaseThePuppy          ---
+------------------------------------
+
+  return(function() local chase = setmetatable if chase then print(textwiththe{symbol) if chase then run{'ch','you','are',moderator,ase,you are using chasefuscator!!,thankyou,this,is,a,strong,obfuscator,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z} table.unpack or g.unpack or getfenv() or _ENV
+
+
+          --chaseluacode(this is the code for chaselua runner to run its servers
+
+          {"YXF1YSBza3llIGFxdWEgY2hhc2UgYXF1YSByb2NreSBhcXVhYWx1YSBhcXVhIHdlYnMgYXF1YSBjaGFzZXRoZXB1cHB5IHNoaGhoZWJldWJkbnNqYmV5Z2R2eWVkdmpoZWJzeXZzaGJha2puc2JlZGhiZXVkaGJ3bnVidWJ1YmV1YmR1YmVkeXdic3doc2tldWhlZmh5YmU3cmY3eXJmYjN1NGZiNzgzNGYzN2Y3MzRmNzc3Nzc3Nzc0cmY3N2I3YnI3YjNyZmhrZGZqaHdoZjMyaHVyeTMyNHlyeXUzZnUzNGYzNGZ5cmZncmZ5cmZ5cmZ5ZXlkeWdlZHlncnlkZ3YgZnVuY3Rpb24gQ2hhc2U6aW5pdCgpIGNoYXNlZ2V0d2lub3d3YmRoYmVoZ3N4aGVndmVoZ3ZkZ2VkY2VndmZkamhzaGplaGd2ZGpzamUzeTc4M2hlbmVodTRvZmhlaXVmcmllamZmW11lW2ZlXXJmW3JdZWZlZltyZltmZVtlcl1bZmVyXVtmZV1bcmZdW2VbZl1lcl1bZnRnXVtlcnRdW2UzW3Jla3lmMWU0MjU3NjEyOTgzNTgyOTU4MjM0Njg3NTY4Mzg2NDg0ODUzNjg5ODJocjM4NzQ4NzMyNDg0ODc3M2c0M3Q3MzR0NjdnNDN0Nmc3MzQ3Z3Q2MzdnNnQzcjc2ZzNydDYzcjZ0N2czcjY3dDQ0MzZ0ZzNyNzZncjRnOGczNDg4M3I0cjh0NHI4dDQzODQzOHl0M3Q4eTM0OHl0MzQ4MzQ4eTN5NDh5aXMgdHJ5ZXVqZWRiYmU="}
