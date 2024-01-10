546efcb149511661a3cc803dbed082ee6de47df9
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-01-10 13:35:53 +0500

    Create chaselua-config-run.lua

---
+++ chaselua-config-run.lua
@@ -0,0 +1,18 @@
+-------------------
+---chasethepuppy---
+-------------------
+
+
+ChaseLua = {}
+   Title = 'Chasefuscator'
+      true
+          --import Py.Util
+    try: 92190,436,4,6,4,7,57 
+           getfENV_()
+      --this part is not free to edit \n
+           --as it is obfuscated, if this part was decrypted, the whole
+            --obfuscator would not work
+local ps = setchasecode
+return(function() local ps = setchasecode if ps then get g.unpack or table.unpack or g.decrypt = true
+        --local ps
+       return(function() setChasecode, if 0 then end then write{'ch','fus','as','e','cat','or'} local a = 1 if a then write func{'ar','you','e','
