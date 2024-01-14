b116c1b0ca129bf1c8fd3df05fb97838e3b7509e
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-01-14 14:57:59 +0500

    Create cg.lua

---
+++ l/l/u/u/a/cg.lua
@@ -0,0 +1,3 @@
+{'https://github.com/ChaseThePuppy/Chasefuscator-v0.1/actions/runs/7518388838/job/20465672123'}
+{'https://github.com/ChaseThePuppy/Chasefuscator-v0.1/actions/runs/7518388838/job/20465674931'}
+while do true end end
