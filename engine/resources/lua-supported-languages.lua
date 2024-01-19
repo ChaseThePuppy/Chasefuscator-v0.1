d1226451c62304ebb3638ee0a3b730904e436278
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-01-19 10:38:04 +0500

    Create lua-supported-languages.lua

---
+++ engine/resources/lua-supported-languages.lua
@@ -0,0 +1,28 @@
+--[[
+
+╔═══╦╗───────────╔═╗───────────╔╗
+║╔═╗║║───────────║╔╝──────────╔╝╚╗
+║║─╚╣╚═╦══╦══╦══╦╝╚╦╗╔╦══╦══╦═╩╗╔╬══╦═╗
+║║─╔╣╔╗║╔╗║══╣║═╬╗╔╣║║║══╣╔═╣╔╗║║║╔╗║╔╝
+https://discord.gg/umuj2jPx5T
+║╚═╝║║║║╔╗╠══║║═╣║║║╚╝╠══║╚═╣╔╗║╚╣╚╝║║
+╚═══╩╝╚╩╝╚╩══╩══╝╚╝╚══╩══╩══╩╝╚╩═╩══╩╝
+]]--
+
+local 1,2,3,4,5,6,7,8,9,0,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+--[[lililil]] while do a then do print('returning')
+    return 1
+     return 2
+     return 3 
+     return 4 
+      return 5
+      return 6
+       return 7
+       return 8 
+      return 9
+      return 0
+    return __ALL__
+       --[[--all-- config]]
+            while do _all__ then require the all files if not while do __all__ then do not require all files
+              lua version: 0xchaseVersionx0
